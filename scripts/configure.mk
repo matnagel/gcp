ca91c106b0a32e5f24971c459f6ea9da89951a6a
Matthias Nagel <matthinagel@gmail.com>
2022-04-23 17:07:46 +0200

    Adds makefile

---
+++ scripts/configure.mk
@@ -0,0 +1,17 @@
+.PHONY: all terraform check-env
+
+all: terraform
+
+terraform: terraform/env/backend.conf terraform/env/custom.tvars
+
+terraform/env/backend.conf: check-env
+	gsutil -q cp gs://$CONFIG_BUCKET/terraform-backend.conf terraform/env/backend.conf
+
+terraform/env/custom.tvars: check-env
+	gsutil -q cp gs://$CONFIG_BUCKET/terraform-custom.tvars terraform/env/custom.tvars
+
+check-env:
+ifndef CONFIG_BUCKET
+	$(error CONFIG_BUCKET is undefined)
+endif
+
--- scripts/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-[ -z "$CONFIG_BUCKET" ] && echo "Need to set CONFIG_BUCKET" && exit 1;
-echo "Loading terraform configuration from bucket: $CONFIG_BUCKET"
-gsutil -q cp gs://$CONFIG_BUCKET/terraform-backend.conf terraform/env/backend.conf
-gsutil -q cp gs://$CONFIG_BUCKET/terraform-custom.tvars terraform/env/custom.tvars
