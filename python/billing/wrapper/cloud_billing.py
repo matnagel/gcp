b69029db192de2c0a2cc31277838549d2d83875d
Matthias Nagel <matthinagel@gmail.com>
2022-04-25 17:01:15 +0200

    Executes requests

--- python/billing/wrapper/cloud_billing.py
+++ python/billing/wrapper/cloud_billing.py
@@ -13,8 +13,16 @@ class CloudBilling:
     def get_projects(self) -> Set[str]:
         billing_projects_api = self.billing_accounts_api.projects()
         billing_id = f"billingAccounts/{self.billing_id}"
-        projects = billing_projects_api.list(name=billing_id)
-        return set(projects)
+
+        projects: Set[str] = set()
+
+        request = billing_projects_api.list(name=billing_id, pageToken="").execute()
+        nextToken = request["nextPageToken"]
+
+        print(f"Request: {request}")
+        print(f"nextToken: {nextToken}")
+
+        return projects
 
     def disable_billing(self, project_id: str) -> None:
         project_name = f"projects/{project_id}"
