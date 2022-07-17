61923f2b329efb5a663cef4aebf1afe5e03bce85
Matthias Nagel <matthinagel@gmail.com>
2022-07-17 12:01:25 +0200

    Adds labels

--- terraform/modules/webpage/main.tf
+++ terraform/modules/webpage/main.tf
@@ -2,5 +2,6 @@ resource "google_storage_bucket" "build-logs-bucket" {
   name     = "${var.project}-build-logs"
   project = var.project
   location = var.region
+  labels = {}
   uniform_bucket_level_access = true
 }
