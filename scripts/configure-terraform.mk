1cc594cd6d00096086b30551ed7d69d3ea158605
Matthias Nagel <matthinagel@gmail.com>
2022-07-20 20:08:51 +0200

    Puts configs in secret folder

--- scripts/configure-terraform.mk
+++ scripts/configure-terraform.mk
@@ -4,11 +4,14 @@ all: terraform
 
 terraform: terraform/env/backend.conf terraform/env/custom.tvars
 
-terraform/env/backend.conf: check-env
-	gsutil -q cp gs://${CONFIG_BUCKET}/terraform/terraform-backend.conf terraform/env/backend.conf
+terraform/env/backend.conf: check-env secrets/config
+	gsutil -q cp gs://${CONFIG_BUCKET}/terraform/terraform-backend.conf secrets/config/terraform-backend.conf
 
-terraform/env/custom.tvars: check-env
-	gsutil -q cp gs://${CONFIG_BUCKET}/terraform/terraform-custom.tvars terraform/env/custom.tvars
+terraform/env/custom.tvars: check-env secrets/config
+	gsutil -q cp gs://${CONFIG_BUCKET}/terraform/terraform-custom.tvars secrets/config/terraform-custom.tvars
+
+secrets/config:
+	mkdir -p secrets/config
 
 check-env:
 ifndef CONFIG_BUCKET
