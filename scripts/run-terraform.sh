c405e047172fa36baa9860e3914603ffbaf66add
Matthias Nagel <matthinagel@gmail.com>
2022-07-20 20:10:50 +0200

    Applies plan

--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -4,4 +4,4 @@ set -e
 cd terraform/env
 terraform init -backend-config="../../secrets/config/terraform-backend.conf"
 terraform plan -var-file="../../secrets/config/terraform-custom.tvars" -out deployment-plan
-#terraform apply deployment-plan
+terraform apply deployment-plan
