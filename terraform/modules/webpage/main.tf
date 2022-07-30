f8247b28c2c7aa4e13b418571935a90124a3f3f3
Matthias Nagel <matthinagel@gmail.com>
2022-07-30 17:57:42 +0200

    Adds appengine api

--- terraform/modules/webpage/main.tf
+++ terraform/modules/webpage/main.tf
@@ -1,31 +1,36 @@
 resource "google_storage_bucket" "build-logs-bucket" {
-  name     = "${var.project}-build-logs"
-  project = var.project
-  location = var.region
-  labels = {}
+  name                        = "${var.project}-build-logs"
+  project                     = var.project
+  location                    = var.region
+  labels                      = {}
   uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket" "website-resources" {
-  name     = "${var.project}-website-resources"
-  project = var.project
-  location = var.region
-  labels = {}
+  name                        = "${var.project}-website-resources"
+  project                     = var.project
+  location                    = var.region
+  labels                      = {}
   uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket" "website-configuration" {
-  name     = "${var.project}-website-configuration"
-  project = var.project
-  location = var.region
-  labels = {}
+  name                        = "${var.project}-website-configuration"
+  project                     = var.project
+  location                    = var.region
+  labels                      = {}
   uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket" "website-tf-state" {
-  name     = "${var.project}-tf-state"
-  project = var.project
-  location = var.region
-  labels = {}
+  name                        = "${var.project}-tf-state"
+  project                     = var.project
+  location                    = var.region
+  labels                      = {}
   uniform_bucket_level_access = true
+}
+
+resource "google_project_service" "appengine_api" {
+  project = var.project
+  service = "appengine.googleapis.com"
 }
