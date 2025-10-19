08e4bdef5980170c9ec8f4efa1eccb2969281dd4
Matthias Nagel <matthinagel@gmail.com>
2025-10-19 09:26:45 +0200

    Separate into build and terraform step

--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 set -e
+
+make -f scripts/configure-terraform.mk
+
 cd terraform/env
 terraform init -backend-config="../../secrets/config/terraform-backend.conf"
 terraform plan -var-file="../../secrets/config/terraform-custom.tvars" -out deployment-plan
