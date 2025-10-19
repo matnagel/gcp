084d539625c0a53791c8176817d651a2515697a5
Matthias Nagel <matthinagel@gmail.com>
2025-10-19 09:34:42 +0200

    Revert "Separate into build and terraform step"
    This reverts commit 08e4bdef5980170c9ec8f4efa1eccb2969281dd4.

--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 set -e
-
-make -f scripts/configure-terraform.mk
-
 cd terraform/env
 terraform init -backend-config="../../secrets/config/terraform-backend.conf"
 terraform plan -var-file="../../secrets/config/terraform-custom.tvars" -out deployment-plan
